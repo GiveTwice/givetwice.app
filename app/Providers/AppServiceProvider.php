e4d411627492c9db2f0b6ec12e4abc8e532fef1a
Mattias Geniar <m@ttias.be>
2025-12-16 13:56:45 +0100

    Avoid duplicate events firing (L11 auto-discovers listeners)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,12 +2,7 @@
 
 namespace App\Providers;
 
-use App\Events\GiftCreated;
-use App\Listeners\CreateDefaultListForNewUser;
-use App\Listeners\DispatchGiftDetailsFetch;
-use Illuminate\Auth\Events\Registered;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -26,8 +21,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::preventLazyLoading(! $this->app->isProduction());
-
-        Event::listen(Registered::class, CreateDefaultListForNewUser::class);
-        Event::listen(GiftCreated::class, DispatchGiftDetailsFetch::class);
     }
 }
